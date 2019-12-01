fc1f38723c46ccaefb996c4a5f0b8e7f575423e1
mixasel <mixasel@gmail.com>
2019-12-02 00:05:30 +0500

    head

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.penValue = new System.Windows.Forms.NumericUpDown();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.penValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбрать цвет";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Толщина пера:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(23, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // penValue
+            // 
+            this.penValue.Location = new System.Drawing.Point(12, 177);
+            this.penValue.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.penValue.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.penValue.Name = "penValue";
+            this.penValue.Size = new System.Drawing.Size(80, 20);
+            this.penValue.TabIndex = 3;
+            this.penValue.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(12, 392);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(99, 46);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Очистить область";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(122, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 426);
+            this.panel1.TabIndex = 5;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.penValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.penValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown penValue;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Color color = Color.Black;                      // цвет пера
+        bool isPressed = false;                         // состояние нажатие кнопки
+        Point currentPoint;                             // текущая точка рисунка
+        Point startPoint;                               // начальная точка 
+        Graphics graphics;                              // графический элемент
+        ColorDialog colorDialog = new ColorDialog();    // диалоговое окно для выбора цвета пера
+        public Form1()
+        {
+            InitializeComponent();
+            label3.BackColor = Color.Black;
+            graphics = panel1.CreateGraphics();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //if ( colorDialog.ShowDialog() == DialogResult.OK )
+            //{
+            //    color = colorDialog.Color;
+            //    label3.BackColor = colorDialog.Color;
+            //}
+            DialogResult result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                color = colorDialog.Color;
+                label3.BackColor = colorDialog.Color;
+            }
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true; //нажали
+            currentPoint = e.Location; 
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false; //отпустили
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ( isPressed == true )
+            {
+                startPoint = currentPoint;
+                currentPoint = e.Location;
+                CreatePen();
+            }
+        }
+        private void CreatePen()
+        {
+            Pen pen = new Pen(color, (float)penValue.Value);
+            graphics.DrawLine(pen, startPoint, currentPoint);
+        }
+    }
+}
